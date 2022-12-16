c0e2f7aed7b75ceb18439c0a44cd848a120029d4
DouiriMouheb <douirijet1gmail.com>
2022-12-16 19:55:25 +0100

    solution using python

---
+++ Solution using python/test.py
@@ -0,0 +1,19 @@
+import math
+speed =int  (input("speed ="))
+ttbs=150/speed
+traffic=[10,10,10]
+def f(speed,traffic):
+    res=ttbs
+    for i in range (len (traffic)):
+        if math.floor(res/traffic[i])%2==0:
+            res+=ttbs
+
+        else:
+            x=1-(res/traffic[i]-math.floor(res/traffic[i]))
+            res+=x*traffic[i]
+            res+=ttbs
+           
+            
+    print (res)
+    
+f(speed,traffic)
